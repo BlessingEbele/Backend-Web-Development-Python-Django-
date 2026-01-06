8d3a1aee50759a02571dc2629fc7707762584209
blessingebele <blessmart.com@gmail.com>
2026-01-06 16:53:50 +0100

    tuples.py added and populated

---
+++ python_basics/day_2/tuples.py
@@ -0,0 +1,9 @@
+#Tuples are like lists but cannot be modified. 
+colors = ("red", "blue", "green")
+print(colors[0])
+
+#Trying ti modify: 
+# colors[0] = "yellow" #This causes an error
+
+print(type(colors))
+print(len(colors))
