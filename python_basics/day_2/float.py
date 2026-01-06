e094666185621c05a6e20e11dd7bff178b713a05
blessingebele <blessmart.com@gmail.com>
2026-01-06 14:50:33 +0100

    float.py add

---
+++ python_basics/day_2/float.py
@@ -0,0 +1,6 @@
+#Floats represnt number with decimals. 
+
+temperature = 36.5
+print(temperature)
+
+print(temperature * 6)
---
+++ python_basics/day_2/integers.py
@@ -0,0 +1,10 @@
+#intergers represent whole number. positive or negative.
+
+age = 34
+year = 2026
+month = 1
+day = 6
+print(age + year + month)
+
+print(str(age) + str(year) + str(month))
+print(str(day) + str(month) + str(year))
