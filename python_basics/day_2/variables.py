a48a11c5c7d7ca731ed483b28f9ffcec60228a89
blessingebele <blessmart.com@gmail.com>
2026-01-06 13:27:22 +0100

    vairables.py content populated

--- python_basics/day_2/variables.py
+++ python_basics/day_2/variables.py
@@ -1,1 +1,13 @@
-nano
+# nano 
+#we use nano command to created this file 
+# variables: are used to store data values. 
+
+name = "Blessing"
+age = 31
+is_student = True 
+''' name holds a string value "Blessing" 
+age holds an integer value 31
+is_student holds a boolean value True '''
+print(name, age, is_student)
+
+# the variables here are the name, age, is_student. they hold the vale of string, ingterger annd boolean reaspectivly . 
