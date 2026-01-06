0eeecc8369ac051c928a323c689232e3d8c9fbb1
blessingebele <blessmart.com@gmail.com>
2026-01-06 13:45:16 +0100

    string.py added

---
+++ python_basics/day_2/string.py
@@ -0,0 +1,16 @@
+# string represents text surrounded by quotes ("")
+
+message = "Welcome to python!"
+
+print(message)
+
+#string operations:
+#concatenation (joining strings)
+
+first_name = "Blessing"
+last_name = "Anochili"
+full_name = first_name + " " + last_name
+print(full_name)
+
+#lenght of a string
+print(len(full_name))
