89ab54794f77cdf305f5a7395d5c02e580887538
blessingebele <blessmart.com@gmail.com>
2026-01-06 18:45:12 +0100

    exercise 1day 2

---
+++ python_basics/day_2/exercises/bio.py
@@ -0,0 +1,32 @@
+#📝 Exercise 1 — Create a Simple Bio
+'''Create a file: bio.py
+Include:
+Name
+
+
+Age
+
+
+A boolean (e.g., has_children)
+
+
+A list of hobbies
+
+
+Print them one by one.
+'''
+
+
+name = "Blessing"
+age = 31
+has_children = True
+hobbies = ["coding", "reading", "traveling", "teaching","singing"]
+
+data = {
+    "name": name,
+    "age": age,
+    "has_children": has_children,
+    "hobbies": hobbies
+}
+print(data)
+print(type(data))
