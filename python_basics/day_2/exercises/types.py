e4f01bc201a0e826adcfb7a95a75231fc1d70c66
blessingebele <blessmart.com@gmail.com>
2026-01-06 22:15:21 +0100

    types.py file populated

--- python_basics/day_2/exercises/types.py
+++ python_basics/day_2/exercises/types.py
@@ -1,5 +1,5 @@
 '''
-Ì≥ù Exercise 4 ‚Äî Data Type Inspector (Optional, For Practice)
+ÔøΩÔøΩÔøΩ Exercise 4 ‚Äî Data Type Inspector (Optional, For Practice)
 Create a file: types.py
 Ask Python to print the data type of different variables:
 print(type(10))
@@ -12,3 +12,18 @@ print(type(True))
 
 '''
 
+name = "Blessing"
+age = 25
+is_active = True
+float_number = 3.14
+items = ["apple", "banana", "orange"]
+items2 = {"apple", "banana", "orange"}
+items3 = ("apple", "banana", "orange")
+print(type(name))
+print(type(age))
+print(type(is_active))
+print(type(float_number))
+print(type(items))
+print(type(items2))
+print(type(items3))
+print(type("Blessing"))
