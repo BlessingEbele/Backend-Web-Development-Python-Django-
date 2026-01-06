95b898750b7fe0b0b2f59c1875069eb0d2485a39
blessingebele <blessmart.com@gmail.com>
2026-01-06 13:57:46 +0100

    string.py file updated with neww contents

--- python_basics/day_2/string.py
+++ python_basics/day_2/string.py
@@ -14,3 +14,19 @@ print(full_name)
 
 #lenght of a string
 print(len(full_name))
+print(len(first_name))
+print(len(last_name))
+print(len(message))
+
+
+print("The lenght of the full name is:", len(full_name))
+print("The lenght of the last name is:", len(last_name))
+print("The lenght of the fisrt name is:", len(first_name))
+print("The lenght of the message is:", len(message))
+
+
+#changing case of a string
+
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
