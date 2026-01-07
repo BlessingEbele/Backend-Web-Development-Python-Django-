1da1062b90d8ea108b4091dcb2bc5c15241d9c31
blessingebele <blessmart.com@gmail.com>
2026-01-07 17:04:11 +0100

    dictionaries.py file created

---
+++ week1_python_basics/youtube_tutorials/dictionaries.py
@@ -0,0 +1,2 @@
+#we will begin learning about dictionaries. Dictionaries allow us to work with key-value pairs in Python. We will go over dictionary methods, how to add and remove values, and also how to loop through the key-value pairs. Let's get started.
+
