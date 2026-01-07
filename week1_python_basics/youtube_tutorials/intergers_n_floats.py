c457fdb59ff31f3bab14cf2b4cf289537a0dfb0f
blessingebele <blessmart.com@gmail.com>
2026-01-07 15:35:18 +0100

    intger and float .py file updated

--- week1_python_basics/youtube_tutorials/intergers_n_floats.py
+++ week1_python_basics/youtube_tutorials/intergers_n_floats.py
@@ -34,3 +34,41 @@ lets ee it in action.
 num = 5
 num *= 6
 print(num)
+print(abs(-3)) # the absolute vale of -3 will be printed out
+print(round(3.78)) #this rounds 3.78 to a whole number which is 4
+print(round(3.78, 1)) # this is passing of an argument telling the computer to round the nume to one decimal place. which will now give 3.8 instaed. 
+#another use example is 
+print(round(3.4567, 2)) # this will print out 3.46 to the console. leaving the number to two decila palces. 
+
+#the use of comparision in python
+'''
+egauls: 6 == 2
+not equal: 6 != 2
+greater than: 3 > 2
+less than: 3 < 2
+greater or equal: 3 >= 2
+less or equal: 3 <= 2
+'''
+
+num_1 = 3
+num_2 = 2
+
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+
+num_1 = '100'
+num_2 = '200'
+print(num_1 + num_2) # this will concatenate the value and print it out as string.
+''' to print the result out as an integer we convert the 
+ to integer using 'int'
+'''
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+#now it add the numbers together and print the resul out as integer
+
+print(num_1 + num_2)
