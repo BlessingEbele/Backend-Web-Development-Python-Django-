9b583948c8da659392ed2c1eaeab7bc1f9002c01
blessingebele <blessmart.com@gmail.com>
2026-01-11 11:14:35 +0100

    combining conditions py file added

---
+++ week1_python_basics/day_3/Combining_Conditions.py
@@ -0,0 +1,18 @@
+# #Combining Conditions
+# '''
+# Use logical operators:
+# Operator        Meaning
+# and             both conditions must be True
+# or              at least one condition must be True
+# not             reverses True/False
+# '''
+
+# #Example:
+# age = 25
+# has_id = True
+
+# if age >= 18 and has_id:
+#     print("Entry allowed.")
+# else:
+#     print("Entry denied.")
+
