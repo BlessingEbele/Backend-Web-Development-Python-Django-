daad10aeeec90549885cbd19423c0408a1713f01
blessingebele <blessmart.com@gmail.com>
2026-01-11 14:11:41 +0100

    login.py code added and tested

--- week1_python_basics/day_3/exercises/login.py
+++ week1_python_basics/day_3/exercises/login.py
@@ -10,3 +10,14 @@ Ask user to input theirs
 Validate using if/else
 
 '''
+
+Username = 'cynthia'
+password = '1235'
+
+Entered_Username = input("Enter your username: ")
+Entered_password = input('Enter your password: ')
+
+if Entered_Username == Username and password == Entered_password:
+    print('Login Successful')
+else: 
+    print('Invalid username or password')
