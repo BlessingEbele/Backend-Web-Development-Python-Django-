c44136f9e00a09eb7e83e710e48889d883df585e
blessingebele <blessmart.com@gmail.com>
2026-01-11 22:15:22 +0100

    bank.py file populated

--- week1_python_basics/day_3/exercises/bank.py
+++ week1_python_basics/day_3/exercises/bank.py
@@ -12,3 +12,12 @@ Try adding a second condition:
  user_is_verified = True
 '''
 
+account_balance = 501
+user_is_verified = False
+
+if account_balance > 500 and user_is_verified:
+    print('You can withdraw.')
+elif account_balance <= 500:
+    print('Insufficient funds.')
+else:
+    print('Account not verified. Please verify your account first.')
