f78a188fe04469318ae8b766adf5a0f741168adc
blessingebele <blessmart.com@gmail.com>
2026-01-11 14:02:01 +0100

    even_odd.py file code added and tested

--- week1_python_basics/day_3/exercises/even_odd.py
+++ week1_python_basics/day_3/exercises/even_odd.py
@@ -13,3 +13,9 @@ Hint:
 if number % 2 == 0:
 '''
 
+number = int(float(input('Enter a number: ')))# this line can take both floating number and int because it coverts the input to both float and int ant work with them both.
+number = int(input('enter a number: '))#this line of code caanot take a floating number as input. it only convert the input to int and work with int.
+if number % 2 == 0:
+    print('even number')
+else:
+    print('Odd number')
