a0f805715138443507b9dae34116786fbbdd993c
blessingebele <blessmart.com@gmail.com>
2026-01-11 13:49:49 +0100

    age group file code written and tested ok

--- week1_python_basics/day_3/exercises/age_group.py
+++ week1_python_basics/day_3/exercises/age_group.py
@@ -20,3 +20,58 @@ Rules:
 
 '''
 
+age = 61
+
+
+if age >= 0 and age <= 12:
+    print('you are a child')
+elif age >= 13 and age <= 19:
+    print(' you are a teen')
+elif age == 20 or age <= 35:
+    print('Young adult')
+elif age == 36 or age <= 60:
+    print('you are an adult')
+# elif age <= 60:
+else:
+    print(' you are a senior')
+# else:
+    # print('you will leave longer')
+
+
+#another way
+
+age = int(input('what is your age:?'))
+
+if age >= 0 and age <= 12:
+    print('you are a child')
+elif age >= 13 and age <= 19:
+    print(' you are a teen')
+elif age >= 20 or age <= 35:
+    print('Young adult')
+elif age >= 36 or age <= 60:
+    print('you are an adult')
+# elif age <= 60:
+else:
+    print(' you are a senior')
+# else:
+    # print('you will leave longer')
+
+#OR
+age = int(input('how old are you?: '))
+
+if age < 0:
+    print('invalid age')
+elif age <= 12:
+    
+    print('you are a child')
+elif age <= 19:
+    print(' you are a teen')
+elif age <= 35:
+    print('Young adult')
+elif age <= 60:
+    print('you are an adult')
+# elif age <= 60:
+else:
+    print(' you are a senior')
+# else:
+    # print('you will leave longer')
