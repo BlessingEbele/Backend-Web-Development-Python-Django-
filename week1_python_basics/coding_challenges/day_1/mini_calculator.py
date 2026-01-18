e69369bbfe5b517ab3b51fc2f045e4c1ae0dfd95
blessingebele <blessmart.com@gmail.com>
2026-01-18 22:51:23 +0100

    mini_calculator.py file populated

--- week1_python_basics/coding_challenges/day_1/mini_calculator.py
+++ week1_python_basics/coding_challenges/day_1/mini_calculator.py
@@ -3,7 +3,7 @@
 Challenge 4: Simple Terminal Calculator
 
 Create:
-í±‰ mini_calculator.py
+ï¿½ï¿½ï¿½ mini_calculator.py
 
 Make Python print the result of:
 
@@ -24,3 +24,8 @@ Subtraction: 27
 Goal: Arithmetic + clean output. 
 '''
 
+a = (15 + 10)
+b = (50 - 23)
+c = (6 * 7)
+d = (100 / 4)
+print(f"Addition: {a}, Subtraction: {b}, Multiplication: {c}, Division; {d}.")
