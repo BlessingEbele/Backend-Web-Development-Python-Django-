0467e3f2f21ef53d6621cd5f978d7936dec1a9b6
blessingebele <blessmart.com@gmail.com>
2026-01-18 22:32:19 +0100

    backend_roles.py file populated

---
+++ week1_python_basics/coding_challenges/day_1/backend_roles.py
@@ -0,0 +1,31 @@
+'''
+Challenge 5: Backend Concept Printer
+
+Create:
+��� backend_roles.py
+
+Print 5 backend responsibilities, each on a new line:
+
+User authentication
+
+Database management
+
+APIs
+
+Security
+
+Server logic
+
+Goal: Reinforce backend theory while coding.
+'''
+role1 = "User authentication"
+role2 = "Database management"
+role3 = "APIs"
+role4 = "Security"
+role5 = "Server logic"
+print(f"Here are five backend roles you need to know: first is to integrate {role1}. ")
+print(f"sencondly to control {role2} system.")
+print(f"To build scalable {role3}.")
+print(f"To enhance {role4}.")
+print(f"To control and write the {role5}.")
+
