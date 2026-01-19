18bdc58e4938cfc5ae546cd9175d45c8daa17426
blessingebele <blessmart.com@gmail.com>
2026-01-19 21:38:42 +0100

    number_type.py file populated

--- week1_python_basics/coding_challenges/day_3/number_type.py
+++ week1_python_basics/coding_challenges/day_3/number_type.py
@@ -2,20 +2,7 @@
 Challenge 8: Positive, Negative or Zero
 
 Create:
-í±‰ number_type.py
-
-Store a number and print whether it is:
-
-Positive
-
-Negative
-
-Zero
-
-Concept: Multiple conditionsChallenge 8: Positive, Negative or Zero
-
-Create:
-í±‰ number_type.py
+ï¿½ï¿½ï¿½ number_type.py
 
 Store a number and print whether it is:
 
@@ -28,3 +15,10 @@ Zero
 Concept: Multiple conditions
 '''
 
+number = -1
+if number < 0:
+    print("it's a negetive number")
+elif number == 0:
+    print("The number is 0")
+else:
+    print("it's a positive number")
