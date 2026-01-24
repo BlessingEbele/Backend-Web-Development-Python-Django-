0a7ee5c70f12761d1ed2730187bf344af8294cce
blessingebele <blessmart.com@gmail.com>
2026-01-24 16:43:34 +0100

    setoperation.py file updated

--- week1_python_basics/day_4/exercises/DictionaryPractice.py
+++ week1_python_basics/day_4/exercises/DictionaryPractice.py
@@ -2,21 +2,24 @@
 ✅ Exercise 4 — Dictionary Practice
 Create a dictionary for a product with:
 name
-
-
 price
-
-
 quantity
-
-
 Then:
 update the price
-
-
 add a "category"
-
-
 print all keys and values
 '''
 
+product_dictionary = {
+    "name": "grape juice",
+    "price": 150,
+    "quantity" : 100,
+}
+
+#update 
+product_dictionary["price"] = 200
+product_dictionary["category"] = "fruit juice"
+print(f"the new product: {product_dictionary}")
+
+for key, value in product_dictionary.items():
+    print(key, ":", value)
--- week1_python_basics/day_4/exercises/SetOperation.py
+++ week1_python_basics/day_4/exercises/SetOperation.py
@@ -6,7 +6,7 @@ Find common items
 
 Find items only in the first set
 '''
-names = {"blessing", "Nora", "Eunice"}
+names = {"Blessing", "Nora", "Eunice"}
 age = {23, 45, 25}
 names2 = {"James", "Blessing", "Chike"}
 age2 = {23, 34, 67}
@@ -17,7 +17,7 @@ print(f"The Difference between the first age and the second age: {age.difference
 print(f"The Difference between the first name and the second name:{names.difference(names2)}")
 
 #find the common item
-print(f"The interserction of the first age and the second age:{age.intersection (age2)}")
-print(f"The intersection of the first name and the second name:{names.intersection(names2)}")
+print(f"The interserction of the first age and the second age:{age.intersection (age2)}")# what they have in common 
+print(f"The intersection of the first name and the second name:{names.intersection(names2)}")# what they have in common
 
 
