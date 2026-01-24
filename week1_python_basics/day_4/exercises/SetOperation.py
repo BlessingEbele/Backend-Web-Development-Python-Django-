f4e4be9709e362935d030a116f3d387ce853708d
blessingebele <blessmart.com@gmail.com>
2026-01-24 16:20:42 +0100

    setoperatio.py file updated

--- week1_python_basics/day_4/exercises/SetOperation.py
+++ week1_python_basics/day_4/exercises/SetOperation.py
@@ -5,7 +5,19 @@ Find common items
 
 
 Find items only in the first set
+'''
+names = {"blessing", "Nora", "Eunice"}
+age = {23, 45, 25}
+names2 = {"James", "Blessing", "Chike"}
+age2 = {23, 34, 67}
 
+print(f"The u ion of first name and the second name: {names.union (names2)}") 
+print(f"The union of the first age and the second age: {age.union(age2)}")
+print(f"The Difference between the first age and the second age: {age.difference(age2)}")
+print(f"The Difference between the first name and the second name:{names.difference(names2)}")
 
+#find the common item
+print(f"The interserction of the first age and the second age:{age.intersection (age2)}")
+print(f"The intersection of the first name and the second name:{names.intersection(names2)}")
 
 
