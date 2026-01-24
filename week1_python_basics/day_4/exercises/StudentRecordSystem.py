00c6ae088d5960bc320c84a1e0b0260cd088517e
blessingebele <blessmart.com@gmail.com>
2026-01-24 21:43:33 +0100

    studentrecordsystem.py populated

--- week1_python_basics/day_4/exercises/StudentRecordSystem.py
+++ week1_python_basics/day_4/exercises/StudentRecordSystem.py
@@ -2,28 +2,26 @@
 Mini-Project of the Day — “Student Record System”
 Requirements
 Create a dictionary representing a student.
-
-
 The student has:
-
-
 name
-
-
 age
-
-
 list of courses
-
-
 a nested dictionary for scores
-
-
 Add a new score
-
-
 Print the highest score
 
-
 '''
+student = {
+    "name": "Blessing",
+    "age": 30,
 
+    "list of courses" :["information technology", "anatomy"],
+    "scores": {
+        "information technology": 80,
+        "anatomy": 95,
+    }
+}
+
+student["scores"]["computer technology"]= 85
+print("Student Record:", student)
+print("Highest score: ", max(student["scores"].values()))
