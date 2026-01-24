916479fd5f25c4164746b26f43ddfbe97384939c
blessingebele <blessmart.com@gmail.com>
2026-01-24 11:16:56 +0100

    tupleconvertion.py file populated

--- week1_python_basics/day_4/exercises/TupleCovertion.py
+++ week1_python_basics/day_4/exercises/TupleCovertion.py
@@ -3,3 +3,14 @@
 Take a list, convert to tuple, try modifying it (see error).
 '''
 
+countries = ["Ghana", "Japa", "Nigeria", "China", "Senegal"]# this is a list
+print(f"the list: {countries}")
+print(type(countries))
+
+#Convert to tuple
+countries_tuple = tuple(["Ghana", "Japa", "Nigeria", "China", "Senegal"])
+print(type(countries_tuple))
+print(f"the list converted to tuple: {countries_tuple}")
+
+#try modifying
+countries_tuple[0] = "USA" #this will rasie TypeError
