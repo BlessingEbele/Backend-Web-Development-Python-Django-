88f561c0db5e206b202e838860babc549d04f4c2
blessingebele <blessmart.com@gmail.com>
2026-01-26 17:19:32 +0100

    unique_name.py file populated

---
+++ week1_python_basics/coding_challenges/day_4/set_op.py
@@ -0,0 +1,21 @@
+'''
+challenge 8: set operation
+Create:
+��� set_ops.py
+
+Given:
+
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+
+Print:
+
+Union
+
+Intersection
+
+Difference (a - b)
+
+Concepts: set operations
+'''
--- week1_python_basics/coding_challenges/day_4/unique_names.py
+++ week1_python_basics/coding_challenges/day_4/unique_names.py
@@ -2,7 +2,7 @@
 Challenge 7: Unique Names (Sets)
 
 Create:
-í±‰ unique_names.py
+ï¿½ï¿½ï¿½ unique_names.py
 
 Given:
 
@@ -13,24 +13,12 @@ Convert the list to a set and print the unique names.
 
 Concepts: removing duplicates using set
 
-Challenge 8: Set Operations
-
-Create:
-í±‰ set_ops.py
-
-Given:
-
-a = {1, 2, 3, 4}
-b = {3, 4, 5, 6}
 
 
-Print:
-
-Union
-
-Intersection
-
-Difference (a - b)
-
-Concepts: set operations
 '''
+
+names = ["John", "Mary", "John", "Aisha", "Mary"]
+
+unique_names = set(names)
+print(type(unique_names))
+print(f"Unique names: {unique_names}")
